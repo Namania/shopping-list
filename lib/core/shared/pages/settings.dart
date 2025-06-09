72cf88a5bf5327bd68e5aeccf92f9f642506db02
Namania <mael.namania@gmail.com>
2025-06-10 00:50:02 +0200

    :art: dropdown menu

--- lib/core/shared/pages/settings.dart
+++ lib/core/shared/pages/settings.dart
@@ -1,14 +1,40 @@
+import 'dart:collection';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopping_list/core/shared/cubit/theme_cubit.dart';
 import 'package:shopping_list/core/shared/widget/settings_item.dart';
 
+typedef MenuEntry = DropdownMenuEntry<String>;
+
 class Settings extends StatelessWidget {
   const Settings({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<MenuEntry> themeMenuEntries = UnmodifiableListView<MenuEntry>(
+      ThemeMode.values.map<MenuEntry>(
+        (ThemeMode mode) => MenuEntry(
+          value: mode.name,
+          labelWidget: Text(mode.name),
+          label: mode.name,
+        ),
+      ),
+    );
+    String themeMenuEntriesValue = context.read<ThemeCubit>().state.name;
+
+    List<MenuEntry> langMenuEntries = UnmodifiableListView<MenuEntry>(
+      context.supportedLocales.map<MenuEntry>(
+        (Locale lang) => MenuEntry(
+          value: lang.languageCode,
+          labelWidget: Text(lang.languageCode),
+          label: lang.languageCode,
+        ),
+      ),
+    );
+    String langMenuEntriesValue = context.locale.languageCode;
+
     return Scaffold(
       appBar: AppBar(
         leading: BackButton(),
@@ -24,51 +50,42 @@ class Settings extends StatelessWidget {
             SettingsItem(
               icon: Icons.contrast_rounded,
               title: context.tr('core.settings.item.theme'),
-              trailing: IconButton(
-                icon: Icon(Icons.contrast_rounded),
-                onPressed: () {
-                  context.read<ThemeCubit>().toggleThemeMode();
-                  if (context.mounted) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(
-                          context.tr('core.settings.snack.theme', namedArgs: {"theme": context.read<ThemeCubit>().state.name}),
-                          style: TextTheme.of(context).labelLarge,
-                        ),
-                        backgroundColor:
-                            Theme.of(context).colorScheme.surfaceContainer,
-                        duration: Durations.extralong4,
-                      ),
-                    );
+              trailing: DropdownMenu<String>(
+                initialSelection: themeMenuEntriesValue,
+                onSelected: (String? value) {
+                  ThemeMode newThemeMode;
+                  switch (value) {
+                    case "light":
+                      newThemeMode = ThemeMode.light;
+                      break;
+                    case "dark":
+                      newThemeMode = ThemeMode.dark;
+                      break;
+                    default:
+                      newThemeMode = ThemeMode.system;
+                      break;
                   }
+                  context.read<ThemeCubit>().selectThemeMode(newThemeMode);
                 },
+                dropdownMenuEntries: themeMenuEntries,
+                selectedTrailingIcon: Icon(Icons.arrow_drop_up_rounded),
+                trailingIcon: Icon(Icons.arrow_drop_down_rounded),
               ),
             ),
             SettingsItem(
               icon: Icons.language,
               title: context.tr('core.settings.item.lang'),
-              trailing: IconButton(
-                icon: Icon(Icons.language_rounded),
-                onPressed: () {
-                  context.setLocale(
-                    context.locale.toString() == 'en'
-                        ? Locale('fr')
-                        : Locale('en'),
-                  );
-                  if (context.mounted) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(
-                          context.tr('core.settings.snack.lang', namedArgs: {"lang": context.locale.toString()}),
-                          style: TextTheme.of(context).labelLarge,
-                        ),
-                        backgroundColor:
-                            Theme.of(context).colorScheme.surfaceContainer,
-                        duration: Durations.extralong4,
-                      ),
-                    );
+              trailing: DropdownMenu<String>(
+                initialSelection: langMenuEntriesValue,
+                onSelected: (String? value) {
+                  if (value != null &&
+                      context.supportedLocales.contains(Locale(value))) {
+                    context.setLocale(Locale(value));
                   }
                 },
+                dropdownMenuEntries: langMenuEntries,
+                selectedTrailingIcon: Icon(Icons.arrow_drop_up_rounded),
+                trailingIcon: Icon(Icons.arrow_drop_down_rounded),
               ),
             ),
           ],
--- lib/core/shared/widget/settings_item.dart
+++ lib/core/shared/widget/settings_item.dart
@@ -15,10 +15,13 @@ class SettingsItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        leading: Icon(icon),
-        title: Text(title),
-        trailing: trailing,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: ListTile(
+          leading: Icon(icon),
+          title: Text(title),
+          trailing: trailing,
+        ),
       ),
     );
   }
