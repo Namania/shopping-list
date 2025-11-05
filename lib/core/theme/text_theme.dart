e112be8ae51091baf70f09374ef8f183a2b01efd
Namania <mael.namania@gmail.com>
2025-11-05 19:18:07 +0100

    :recycle: revome google font that need internet

--- lib/core/theme/text_theme.dart
+++ lib/core/theme/text_theme.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 TextTheme createTextTheme(
     BuildContext context, String bodyFontString, String displayFontString) {
-  TextTheme baseTextTheme = Theme.of(context).textTheme;
-  TextTheme bodyTextTheme = GoogleFonts.getTextTheme(bodyFontString, baseTextTheme);
-  TextTheme displayTextTheme =
-      GoogleFonts.getTextTheme(displayFontString, baseTextTheme);
-  TextTheme textTheme = displayTextTheme.copyWith(
-    bodyLarge: bodyTextTheme.bodyLarge,
-    bodyMedium: bodyTextTheme.bodyMedium,
-    bodySmall: bodyTextTheme.bodySmall,
-    labelLarge: bodyTextTheme.labelLarge,
-    labelMedium: bodyTextTheme.labelMedium,
-    labelSmall: bodyTextTheme.labelSmall,
+  TextTheme baseTextTheme = Theme.of(context).textTheme.apply(fontFamily: 'Montserrat');
+  TextTheme textTheme = baseTextTheme.copyWith(
+    bodyLarge: baseTextTheme.bodyLarge,
+    bodyMedium: baseTextTheme.bodyMedium,
+    bodySmall: baseTextTheme.bodySmall,
+    labelLarge: baseTextTheme.labelLarge,
+    labelMedium: baseTextTheme.labelMedium,
+    labelSmall: baseTextTheme.labelSmall,
   );
   return textTheme;
 }
